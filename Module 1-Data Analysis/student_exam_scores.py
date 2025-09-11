f9566f236f39d3f0aaccd06cb82edd3230ff02da
gianvin <ginamangahassanchezong@gmail.com>
2025-09-11 20:40:57 +0800

    write the code for printing the entire data frame

--- Module 1-Data Analysis/student_exam_scores.py
+++ Module 1-Data Analysis/student_exam_scores.py
@@ -1,3 +1,13 @@
 import pandas as pd
 #Code for loading the data set
-df = pd.read_csv("student_exam_scores.csv")
+df = pd.read_csv(r"C:\Users\Gina S. Ong\Documents\byui\HelloWorld\Module 1-Data Analysis\student_exam_scores.csv")
+print(df.head())
+
+#Code for printing the entire dataframe
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None )
+
+print(df)
+
+#Question 1 How many students sleep 8 hours and above?
+
